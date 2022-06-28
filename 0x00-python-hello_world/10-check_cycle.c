3c98bf8cdbb6e03627fd7668d33a8a307fba260d
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-06-28 21:27:07 +0000

    updates

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include lists.h
+
+/**
+ * check_cycle - check if a singly list contains a circle
+ *
+ * @list: the list to check
+ *
+ * Return: Always 0 if not found, 1 if found
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *current;
+
+current = list;
+ while  
+}
