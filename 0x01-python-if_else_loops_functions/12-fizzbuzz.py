530b0a72ed941ad2a7ea4f5e27e7194a0a0e6619
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-06-29 23:03:03 +0000

    updates

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,9 @@ def fizzbuzz():
             print("{:s}".format("FizzBuzz"), end=' ')
             continue
         elif n % 3 == 0:
-             print("{:s}".format("Fizz"), end=' ')
-             continue
+            print("{:s}".format("Fizz"), end=' ')
+            continue
         elif n % 5 == 0:
-             print("{:s}".format("Buzz"), end=' ')
-             continue
+            print("{:s}".format("Buzz"), end=' ')
+            continue
         print("{:d}".format(n), end=' ')
