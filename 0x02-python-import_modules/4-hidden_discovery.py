8f10b7a06e7195f7695e4adba2c44b2a74d1df4e
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-01 19:42:10 +0000

    added the hidden_discovery file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import  hidden_4
+    hidden_list = dir(hidden_4)
+    for s in hidden_list:
+        if s[0] == "_":
+            continue
+        print("{:s}".format(s))
