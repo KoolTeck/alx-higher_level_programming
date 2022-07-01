fbd9466417a7760775003578c7dbd5986b46e7ae
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-01 04:32:08 +0000

    added program printing args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import  argv
+    av_len = len(argv) - 1
+    sum = 0
+    n = 1
+    if av_len > 0:
+        while av_len >= n:
+            sum += int(argv[n])
+            n += 1
+    print("{:d}".format(sum))
