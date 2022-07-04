3c018d6d645bce5e157db99b95ab9005663af43a
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-04 14:10:53 +0000

    updates

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
+    if len(matrix[0]) != 0:
         for row in matrix:
             for i in range(len(row)):
                 print("{:d} ".format(row[i]), end='')
