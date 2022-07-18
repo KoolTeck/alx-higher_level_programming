991cec830cc1ef21b73c9052c67666f121b8099e
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-18 16:39:33 +0000

    updates

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@ def safe_print_list(my_list=[], x=0):
     nb_printed = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             nb_printed += 1
         except IndexError:
             break
-    if nb_printed > 0:
-        print()
+    print()
     return (nb_printed)
