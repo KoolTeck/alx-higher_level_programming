460138c9be8cea51f63faf09657529ebf4af4a50
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-20 09:32:12 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The Square module docstrings.
+
+This module demonstrates creating a Square class and atrributes
+
+File calling:
+
+        $ python3 0-square.py
+
+"""
+
+
+class Square:
+    """ creates an empty Square class  defining a clas """
+
+    def __init__(self, size):
+         """docstring on the __init__ method.
+
+        Args:
+            size (str): the size of the square
+
+        """
+         self.__size = size
