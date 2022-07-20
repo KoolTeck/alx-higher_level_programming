4dd660262fe115455f45280dc8286d303b83f683
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-20 09:36:06 +0000

    updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -14,10 +14,10 @@ class Square:
     """ creates an empty Square class  defining a clas """
 
     def __init__(self, size):
-         """docstring on the __init__ method.
+        """docstring on the __init__ method.
 
         Args:
             size (str): the size of the square
 
         """
-         self.__size = size
+        self.__size = size
