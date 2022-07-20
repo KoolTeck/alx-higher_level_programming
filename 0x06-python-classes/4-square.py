1595dbb678306bd028ab71302bd906a9ad88709f
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-20 11:14:53 +0000

    added Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""The Square module docstrings.
+
+This module demonstrates creating a Square class and atrributes
+
+File calling:
+
+        $ python3 0-square.py
+
+"""
+
+
+class Square:
+    """ creates an empty Square class  defining a class """
+
+    def __init__(self, size=0):
+        """docstring on the __init__ method.
+
+        Args:
+            size (str): the size of the square
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ gets the current size """
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """ sets the current size """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ compute amd return the current square area
+
+        Returns:
+            the square area
+
+        """
+        return (self.__size ** 2)
