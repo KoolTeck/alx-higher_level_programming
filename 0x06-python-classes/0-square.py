50176076775c5ec96a4dd2e946983c75358a0bc7
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-20 09:17:17 +0000

    added 0. My first square task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The Square module docstrings.
+
+This module demonstrates creating a Square class and atrributes
+
+File calling:
+
+        $ python3 0-square.py
+
+"""
+
+
+class Square:
+    """ creates an empty Square class  defining a clas """
+
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x06-python-classes
+
+Solution to tasks on classes in pyhton
