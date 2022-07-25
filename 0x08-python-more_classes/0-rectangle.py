e8fcfe8a93c8efaaff24ebfbbb7255908ff201a9
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-25 15:50:38 +0000

    added 0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" the rectangle module """
+
+
+class Rectangle:
+    """ the Rectangle class """
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# 0x08-python-more_classes
+
+Solution to tasks on more classes in python
