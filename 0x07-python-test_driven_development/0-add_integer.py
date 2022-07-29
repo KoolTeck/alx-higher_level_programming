c01c7fc45b7fe9a3af1c0c81cc977f5be1e1364c
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-07-29 20:28:16 +0000

    updates

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -27,12 +27,11 @@ def add_integer(a, b=98):
 
     err1 = TypeError("a must be an integer")
     err2 = TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    elif type(b) is float:
-        b = int(b)
-    if type(a) is not int:
+    if a is None or type(a) is not int and type(a) is not float:
         raise err1
-    elif type(b) is not int:
+    if type(b) is not int and type(b) is not float:
         raise err2
-    return (a + b)
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return (int(a) + int(b))
