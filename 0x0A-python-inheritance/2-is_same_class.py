caf768d1471fab61b8683c36bfe04c375d6a6e2c
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-08-02 15:52:29 +0000

    added 2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" The 2-is_same_class module """
+
+
+def is_same_class(obj, a_class):
+    """ checks whether an object is an instance of a class """
+    
+    return (obj.__class__ is a_class)
+
+
+if __name__ == "__main__":
+    a = 1
+
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+    if is_same_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
