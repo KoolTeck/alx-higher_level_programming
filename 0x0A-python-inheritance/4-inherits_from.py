090ed725771bd5a6a1fc8175374cbc0df94f5f56
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-08-02 18:47:58 +0000

    added 4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" inherit module """
+
+
+def inherits_from(obj, a_class):
+    """
+    checks if an obj is inherited directly or indirectly
+
+    """
+
+    if isinstance(obj, a_class):
+        if type(obj) is not a_class:
+            return True
+    False
+
+
+if __name__ == "__main__":
+    a = 2
+    if inherits_from(a, int):
+        print("{} inherited from class {}".format(a, int.__name__))
+    if inherits_from(a, bool):
+        print("{} inherited from class {}".format(a, bool.__name__))
+    if inherits_from(a, object):
+        print("{} inherited from class {}".format(a, object.__name__))
