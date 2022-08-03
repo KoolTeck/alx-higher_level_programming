5ade8f6df68aa2a4333a2fab96212b12a9975b12
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-08-03 22:19:37 +0000

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,15 @@ import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-filename = "add_item.json"
 
+filename = "add_item.json"
 try:
     arg_lis = load_from_json_file(filename)
 except Exception:
     save_to_json_file([], filename)
 
 if len(sys.argv) > 1:
+    arg_lis = load_from_json_file(filename)
     i = 1
     while (i < len(sys.argv)):
         arg_lis.append(sys.argv[i])
