18bc717f357264fd7d170fd1efbe7a227dac95c3
Adetunji Adebowale <bowaleadetunji@gmail.com>
2022-08-03 22:01:56 +0000

    added 7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" The 7-add_item Module """
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+
+try:
+    arg_lis = load_from_json_file(filename)
+except Exception:
+    save_to_json_file([], filename)
+
+if len(sys.argv) > 1:
+    i = 1
+    while (i < len(sys.argv)):
+        arg_lis.append(sys.argv[i])
+        i += 1
+    save_to_json_file(arg_lis, filename)
