0ec3a6a988ff4d81d5aadebbe5e406c1d773541e
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-09-08 06:07:30 -0700

    corrected 16-no_link.sql file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,10 @@
 --  lists all records of the table second_table of the database hbtn_0c_0
 -- rows without a name value are not shown
 -- Results  displays the score and the name in descending order of score
-SELECT DISTINCT score, name
+SELECT score, name
 FROM second_table
+WHERE
+name IS NOT NULL
+AND
+score IS NOT NULL
 ORDER BY score DESC;
