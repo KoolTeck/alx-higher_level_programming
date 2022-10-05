0356adb5a970c6b316476e8e15a4d90b8802d7ec
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-10-05 14:16:27 -0700

    added a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argv = process.argv;
+const size = parseInt(argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else if (size >= 0) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+}
