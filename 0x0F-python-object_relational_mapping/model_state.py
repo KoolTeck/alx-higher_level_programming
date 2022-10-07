efd59cf85f6659648ab93c9b3f58957d6709e5e5
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-10-07 05:44:15 -0700

    added script connecting to db using SqlAlchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+"""
+contains the class definition of a State and an
+instance Base = declarative_base():
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(
+        Integer, nullable=False,
+        autoincrement=True, primary_key=True,
+        unique=True
+    )
+    name = Column(String(128), nullable=False)
