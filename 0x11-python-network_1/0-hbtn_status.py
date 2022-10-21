2e7cacaad4e4f7972f200f8518832f102cb27e51
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-10-21 15:39:22 -0700

    updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,12 @@
 """ fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-    html = res.read()
-    fmt = "Body response:\n \
-    \t - type: {}\n \
-    \t - content: {}\n \
-    \t - utf8 content: {}\n"
-    print(fmt.format(type(html), html, res.msg), end="")
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        html = res.read()
+        fmt = """Body response:\n \
+\t- type: {}\n \
+\t- content: {}\n \
+\t- utf8 content: {}\n"""
+        print(fmt.format(type(html), html, res.msg), end="")
