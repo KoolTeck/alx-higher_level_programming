e58ac5990eca6635ec762a59c6f94001298a5cdd
BowaleAdetunji <bowaleadetunji@gmail.com>
2022-10-23 15:46:23 -0700

    updates

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(json['id'], json['name']))
-    except requests.exceptions.JSONDecodeError:
+    except ValueError:
         print("Not a valid JSON")
